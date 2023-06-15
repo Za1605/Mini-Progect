00234fabc581e837b3fb22df7a5c246d3fdfb716
YuliiaT <115080417+Za1605@users.noreply.github.com>
2023-06-15 23:46:00 +0300

    five

--- post-details.js
+++ post-details.js
@@ -10,10 +10,28 @@ fetch(`https://jsonplaceholder.typicode.com/posts/${userId}`)
         recursiveBuilder(user, ul);
         block.appendChild(ul);
     });
+function liCreator(key, value, parent) {
+    const li = document.createElement(`li`);
+    li.innerText = `${key} : ${value}`;
+    parent.appendChild(li);
+}
+
+function ulBuilder(key, object, parent){
+}
+
+function recursiveBuilder(object, parent) {
+    for (const key in object) {
+        typeof object[key] === `object`
+            ? ulBuilder(key, object[key], parent)
+            : liCreator(key, object[key], parent)
+    }
+
+}
+
 fetch(`https://jsonplaceholder.typicode.com/comments/${userId}`)
     .then((res) =>res.json())
     .then((user) => {
-        console.log(user)
+        //console.log(user)
         const block = document.getElementsByClassName(`wrap`)[0];
         const ul = document.createElement(`ul`);
         recursiveBuilder(user, ul);
