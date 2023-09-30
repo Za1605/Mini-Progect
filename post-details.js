af2acfcf26ad539bb93543856943508865ad30e9
YuliiaT <115080417+Za1605@users.noreply.github.com>
2023-10-01 00:37:51 +0300

    fourteen

--- post-details.js
+++ post-details.js
@@ -4,56 +4,58 @@
 // - https://jsonplaceholder.typicode.com/posts/POST_ID/comments)
 //const urlParams = new URLSearchParams(window.location.search);
 //const ID = urlParams.get("postId");
-const id = new URL (location.href).searchParams.get(`postsid`);
+const id = new URL (location.href).searchParams.get(`posts`);
+//console.log(id);
 
-
-
- fetch(`https://jsonplaceholder.typicode.com/posts/${id}`)
+ fetch(`https://jsonplaceholder.typicode.com/posts`)
 
     .then(response => response.json())
     .then(post => {
-        let divAllPost1 = document.createElement('div')
-        for (const postTitle in post) {
-            //if (title === postElement.title){
-                let divAllPost = document.createElement('div')
-               divAllPost.classList.add('divAllPost')
-                let ul = document.createElement('ul')
-                for (const key of postElement) {
-                    let li = document.createElement('li')
-                    //li.innerHTML = `${key} ${postElement[key]}`
-                    li.classList.add('li_user')
-                    ul.append(li)
+        let allpost1 = document.createElement('div');
+        for (const postEl of post) {
+            if (id === postEl.id) {
+                let allpost = document.createElement('div');
+                allpost.classList.add('allpost')
+                let ul = document.createElement('ul');
+                for (const key of postEl) {
+                    let li = document.createElement('li');
+                    li.innerHTML = `${key} ${postEl[key]}`;
+                    li.classList.add('li_user');
+                    ul.append(li);
                 }
-               divAllPost1.append(ul);
-               document.body.append(divAllPost1);
-
+                allpost.append(ul);
+                document.body.append(allpost1);
+            }
+        }
 
         fetch(`https://jsonplaceholder.typicode.com/posts/${id}/comments`)
-            .then((response => response.json()))
-            .then(comments =>{
-                let main_comments_post = document.createElement('div')
-                main_comments_post.classList.add('main_comments_post')
-                let comments_post = document.createElement('div')
-                comments_post.classList.add('comments_post')
+            .then(( response => response.json() ))
+            .then(comments => {
 
-                let h1 = document.createElement('h1')
-                h1.innerText = 'Comments Post:'
-                document.body.append(h1)
-                document.body.append(comments_post)
-                for (const element of comments) {
-                    let divComments = document.createElement('div')
-                    let ul = document.createElement('ul')
-                    divComments.classList.add('divComments')
+                let commentsPost = document.createElement('div');
+                commentsPost.classList.add('commentsPost');
+                let commentsPost1 = document.createElement('div');
+                commentsPost1.classList.add('commentsPost1');
+                let h2 = document.createElement('h2');
+                h2.innerText = 'comments of post';
+                document.body.append(h2);
+                document.body.append(commentsPost);
+                for (const value of comments) {
+                    let boxCom = document.createElement('div');
+                    let ul = document.createElement('ul');
+                    boxCom.classList.add('boxCom');
                     for (const key in element) {
 
-                        let li = document.createElement('li')
-                        li.classList.add('li_user')
-                        li.innerHTML = `<b>${key}</b>: ${element[key]}`
-                        ul.append(li)
-                        divComments.append(ul)
+                        let li = document.createElement('li');
+                        li.classList.add('li_user');
+                       li.innerHTML = `<b>${key}</b>: ${value[key]}`;
+                        ul.append(li);boxCom.append(ul);
                     }
-                    main_comments_post.append(divComments)
-                    document.body.append(main_comments_post)
+                   commentsPost.append(boxCom);
+                   document.body.append(commentsPost);
                 }
+            })
 
-            } )
+
+
+        })
