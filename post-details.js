95d44752ac4ee64fc56384eab3ff0f7016fcb14d
YuliiaT <115080417+Za1605@users.noreply.github.com>
2024-01-15 23:20:08 +0200

    16.01

--- post-details.js
+++ post-details.js
@@ -7,22 +7,27 @@
 //
 //
 
-//const id = new URL (location.href).searchParams.get('id');
-let url = new URL(location.href);
-let postId = url.searchParams.get('userId/post');
 
-fetch(`https://jsonplaceholder.typicode.com/posts/${postId}`)
-    .then(response => response.json())
-    .then(posts =>{
-        //console.log(comments);})
-        let ul = document.createElement('ul');
-        for (const post of posts) {
-            let li = document.createElement('li');
-            li.innerHTML = `<li>${post.id}</li><li>${post.name}</li><li>${post.email}</li><li>${detail.body}</li>`;
-            ul.appendChild(li);
-        }
-        document.body.appendChild(ul)
-    });
+//console.log (post)
+//let url = new URLSearchParams(window.location.search);
+
+//let postId = post.get('postId');
+//console.log(postId)
+//console.log(JSON.stringify(postId))
+
+const urlParams = new URLSearchParams(window.location.search);
+const ID = urlParams.get("postId");
+fetch(`https://jsonplaceholder.typicode.com/posts/${ID}`)
+ .then(response => response.json())
+ .then(posts =>{
+     let ul = document.createElement('ul');
+     for (const post of posts) {
+         let li = document.createElement('li');
+         li.innerHTML = '<li>${post.id}</li><li>${post.name}</li><li>${post.email}</li><li>${detail.body}</li>';
+         ul.appendChild(li);
+     }
+     document.body.appendChild(ul);
+ });
 
 
 
